bd815c4818eed2d7fa72b88e646133f57671070d
Tero Saarikivi <tero.saarikivi@outlook.com>
2017-01-12 16:28:44 +0200

    sefver fix

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ var server = restify.createServer()
 
 var port = process.env.PORT || 3001
 
-app.use(function (req, res, next) {
+server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
